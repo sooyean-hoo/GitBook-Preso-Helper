ee4bc25a7555434c64697e68affeb58e3787c354
student0 <noreply@puppet.com>
2020-11-29 13:17:19 +0800

    Fix a bug on keeping in preso mode.

--- GitBook Preso Helper.user.js
+++ GitBook Preso Helper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GitBook Preso Helper
 // @namespace    http://tampermonkey.net/
-// @version      0.1.0.0.13
+// @version      0.1.0.0.15
 // @description  Adapt GitBook for Use as Presention ( arrowkeys= <PrevPage  NextPage > , B= Black BG, W = Wide Mode, P = Toggle for Preso Mode, S = Open Search, O = Open Index (Cacheing) )
 // @author       Hoo Sooyean 何書淵
 // @grant       GM_xmlhttpRequest
@@ -779,13 +779,15 @@ body[ data-maxscreen="1"  ] div[role="complementary"]{
     }
 
     if ( $("div[ class *= wholeContentBody ]") != null ) {
+        let maxscr=$getCookie('maxscreen')
+        if ( maxscr == "" ) { maxscr=-1 ; $setCookie('maxscreen',maxscr, 300) ; }
+        parentpresoObj.dataset.maxscreen = maxscr ;
+                   
         $("body").addEventListener('keydown', function (event) {
 
             const parentpresoObj=$("div[ class *= wholeContentBody ]")
 
-            let maxscr=$getCookie('maxscreen')
-            if ( maxscr == "" ) { maxscr=-1 ; $setCookie('maxscreen',maxscr, 300) ; }
-            parentpresoObj.dataset.maxscreen = maxscr ;
+
 
             if (event.code === 'KeyB' && event.srcElement.tagName != 'INPUT' ) {
                 $("div[ class *= wholeContentBody ]").dataset.maxscreen = 3 ;
